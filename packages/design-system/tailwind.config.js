35cc570e87ebdc2ee64c0b849977296b5770c29c
h1un <h1unha@naver.com>
2025-06-12 20:09:10 +0900

    modify: #1 globals.css, tailwind.config.js

--- packages/design-system/tailwind.config.js
+++ packages/design-system/tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      fontWeight: {
+        'extra-light': '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        'extra-bold': '800',
+        black: '900',
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
